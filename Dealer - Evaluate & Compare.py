12ebb4dcffe53fda603ad6dfccc3ed96f3ccb0b1
Jackson Ball <142324931+JacksonBallers@users.noreply.github.com>
2023-10-31 08:55:37 -0400

    Update Dealer - Evaluate & Compare.py

--- Dealer - Evaluate & Compare.py
+++ Dealer - Evaluate & Compare.py
@@ -3,17 +3,15 @@ player_value = int(input("Player Enter Number: "))
 dealer_score = 0
 player_score = 0
 
-if dealer_value >= player_value:
+if dealer_value > player_value:
     dealer_score += 1
+    print("Dealer Wins this Round")
+elif dealer_value == player_value:
+    dealer_score += 1
+    print("Tie, Dealer Wins Round")
 else:
     player_score += 1
+    print("Player Wins")
 
 print(f"Player Score is {player_score}")
 print(f"Dealer Score is {dealer_score}")
-
-if dealer_score > player_score:
-    print("Dealer Wins this Round")
-elif dealer_score == player_score:
-    print("Tie, Dealer Wins Round")
-else:
-    print("Player Wins")
