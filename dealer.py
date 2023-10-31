d352350e992e70031a4277a39abb2d404048fc70
Jackson Ball <142324931+JacksonBallers@users.noreply.github.com>
2023-10-31 09:11:29 -0400

    Add files via upload

--- dealer.py
+++ dealer.py
@@ -1,10 +1,17 @@
-def check_tie(dealer_value, player_value):
-    if dealer_value == player_value:
-        return "win"
-
-
+dealer_value = int(input("Dealer Enter Number: "))
+player_value = int(input("Player Enter Number: "))
 dealer_score = 0
-# 2 in the parenthesis will be replaced with the actual values of the hands
-if check_tie(2,2) == "win":
+player_score = 0
+
+if dealer_value > player_value:
     dealer_score += 1
-    print(dealer_score)
+    print("Dealer Wins this Round")
+elif dealer_value == player_value:
+    dealer_score += 1
+    print("Tie, Dealer Wins Round")
+else:
+    player_score += 1
+    print("Player Wins")
+
+print(f"Player Score is {player_score}")
+print(f"Dealer Score is {dealer_score}")
